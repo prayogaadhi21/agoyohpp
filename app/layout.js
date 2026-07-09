c0d2088b4a7a234607b371abf772b733468b2502
prayogaadhi21 <139734484+prayogaadhi21@users.noreply.github.com>
2026-07-09 19:35:17 +0700

    Update metadata titles and descriptions

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
 export const metadata = {
-  title: 'Agoyohpp - Sistem Gudang dan POS',
-  description: 'Sistem manajemen gudang dan kasir untuk coffee shop',
+    title: 'Agoyohpp - Sistem AGOYO STOCK dan POS',
+    description: 'Sistem manajemen AGOYO STOCK dan kasir untuk AGOYO',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="id">
-    <body>{children}</body>
-    </html>
-  );
+    return (
+          <html lang="id">
+            <body>{children}</body>
+      </html>
+    );
 }
