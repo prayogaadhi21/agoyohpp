b37421b4caf6dd9de3bb04e61779c27ef722277b
prayogaadhi21 <139734484+prayogaadhi21@users.noreply.github.com>
2026-07-09 19:33:54 +0700

    Modify staff dashboard content and styling
    Updated the content of the staff dashboard page to reflect specific stock information.

--- app/staff/page.js
+++ app/staff/page.js
@@ -1,8 +1,8 @@
 export default function StaffPage() {
-return (
-<main style={{ padding: 40, fontFamily: 'sans-serif' }}>
-<h1>Dashboard Staff</h1>
-<p>Halaman ini akan menampilkan stok di gudang cabang coffee shop. Modul akan dibangun bertahap.</p>
-</main>
-);
+    return (
+          <main style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Dashboard Staff</h1>
+      <p>Halaman ini akan menampilkan stok AGOYO STOCK di cabang AGOYO. Modul akan dibangun bertahap.</p>
+  </main>
+  );
 }
